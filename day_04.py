4f4c50161aef16b605aafe90b38afb45357e8f38
Your Name <you@example.com>
2026-05-28 11:56:16 +0500

    @singleton

--- day_04.py
+++ day_04.py
@@ -1,20 +1,39 @@
+# class Game:
+#     _instance = None
+
+#     def __new__(cls, *args, **kwargs) -> 'Game':
+#         if cls._instance is None:
+#             cls._instance = super().__new__(cls)
+#             cls._instance.initialized = False
+#         return cls._instance
+
+#     def __init__(self, value: str) -> None:
+#         if not self.initialized:
+#             self.value = value
+#             self.initialized = True
+
+
+def singleton(cls):
+    instances = {}
+
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return wrapper
+
+
+@singleton
 class Game:
-    _instance = None
-
-    def __new__(cls, *args, **kwargs) -> 'Game':
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance.initialized = False
-        return cls._instance
-
     def __init__(self, value: str) -> None:
-        if not self.initialized:
-            self.value = value
-            self.initialized = True
+        self.value = value
+
+
 
 
 if __name__ == '__main__':
-    game1 = Game('Первая игра')
+    game1 = Game("Первая игра")
     game2 = Game('Вторая игра')
     print(game1.value)
     print(game2.value)
