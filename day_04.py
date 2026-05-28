146333b78f307f3d3e2aeceeec7d56a00d11d75a
Your Name <you@example.com>
2026-05-28 12:26:52 +0500

    input

--- day_04.py
+++ day_04.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 # def singleton(cls):
 #     instances = {} ## ???
 
@@ -10,6 +12,7 @@
 #     return wrapper
 
 
+
 class Game:
     _instance = None
 
@@ -19,21 +22,43 @@ class Game:
             cls._instance.initialized = False
         return cls._instance
 
-    def __init__(self, value: str) -> None:
+    def __init__(self, value: str, draw: Draw) -> None:
         if not self.initialized:
             self.value = value
             self.initialized = True
+            self.draw = draw
+            self.draw.output(self.value)
+            self.draw.output(self.draw.input("Привет!!!! Как у тебя дела?"))
+
+# Abstract class ??
+class Draw(ABC):
+    @abstractmethod
+    def input(self, value: str) -> None:
+        pass
+
+    @abstractmethod
+    def output(self, value: str) -> None:
+        pass
+    
 
 
-class Draw:
-    pass
+class ConsoleDraw(Draw):
+    def input(self, value: str) -> None:
+        return input(value)
+    def output(self, value: str) -> None:
+        print(value)
 
+class DisplayDraw(Draw):
+    def input(self, value: str) -> None:
+        pass
+    def output(self, value: str) -> None:
+        pass
 
 
 
 if __name__ == '__main__':
-    game1 = Game("Первая игра")
-    game2 = Game('Вторая игра')
+    game1 = Game("Первая игра", ConsoleDraw())
+    game2 = Game('Вторая игра', DisplayDraw())
     print(game1.value)
     print(game2.value)
     print(game1 is game2)
