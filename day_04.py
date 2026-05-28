240104c9b08cf6580a8e69753248bbd15348c3de
Your Name <you@example.com>
2026-05-28 11:42:45 +0500

    singleton

---
+++ day_04.py
@@ -0,0 +1,21 @@
+class Game:
+    _instance = None
+
+    def __new__(cls, value: str) -> 'Game':
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.initialized = False
+        return cls._instance
+
+    def __init__(self, value: str) -> None:
+        if not self.initialized:
+            self.value = value
+            self.initialized = True
+
+
+if __name__ == '__main__':
+    game1 = Game('Первая игра')
+    game2 = Game('Вторая игра')
+    print(game1.value)
+    print(game2.value)
+    print(game1 is game2)
